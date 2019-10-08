9012c0234b62d8fd0dc11c3daa1d105640ae1b2d
johnnypuskar <johnnypuskar@yahoo.com>
2019-10-08 13:23:34 -0400

    Split Game into Game and Board class

---
+++ src/main/java/com/webcheckers/model/Board.java
@@ -0,0 +1,62 @@
+package com.webcheckers.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+    public final static int BOARD_SIZE = 8;
+    private Space[][] board = new Space[BOARD_SIZE][BOARD_SIZE];
+
+    public Board() {
+        for(int i = 0; i < BOARD_SIZE; i++) {
+            for(int j = 0; j < BOARD_SIZE; j++) {
+                board[i][j] = new Space(i, j, (i%2==0) == (j%2==0));
+            }
+        }
+        resetGameBoard();
+    }
+
+    public Board(Space[][] preset) {
+        this.board = preset;
+    }
+
+    public void resetGameBoard() {
+        for(int i = 0; i < BOARD_SIZE; i++) {
+            for(int j = 0; j < BOARD_SIZE; j++) {
+                Space space = board[i][j];
+                if(space.isValid()) {
+                    if(i < 3) {
+                        space.setPiece(new Piece(Piece.Color.WHITE));
+                    }
+                    else if(i >= 5) {
+                        space.setPiece(new Piece(Piece.Color.RED));
+                    }
+                }
+            }
+        }
+    }
+
+    public Board flipped() {
+        Space[][] flipped = new Space[BOARD_SIZE][BOARD_SIZE];
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                flipped[BOARD_SIZE - 1 - i][BOARD_SIZE - 1 - j] = board[i][j];
+            }
+        }
+        return new Board(flipped);
+    }
+
+    public Space[][] getBoard() {
+        return board;
+    }
+
+    public List<Space> iterator() {
+        List<Space> iter = new ArrayList<Space>();
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                iter.add(board[i][j]);
+            }
+        }
+        return iter;
+    }
+}
--- src/main/java/com/webcheckers/model/Game.java
+++ src/main/java/com/webcheckers/model/Game.java
@@ -1,72 +1,30 @@
 package com.webcheckers.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Game {
-    private final static int BOARD_SIZE = 8;
-    private Space[][] board = new Space[BOARD_SIZE][BOARD_SIZE];
+    private Board board;
 
     public Game() {
-        for(int i = 0; i < BOARD_SIZE; i++) {
-            for(int j = 0; j < BOARD_SIZE; j++) {
-                board[i][j] = new Space(i, j, (i%2==0) == (j%2==0));
-            }
-        }
-        resetGameBoard();
+        this.board = new Board();
     }
 
-    public void resetGameBoard() {
-        for(int i = 0; i < BOARD_SIZE; i++) {
-            for(int j = 0; j < BOARD_SIZE; j++) {
-                Space space = board[i][j];
-                if(space.isValid()) {
-                    if(i < 3) {
-                        space.setPiece(new Piece(Piece.Color.WHITE));
-                    }
-                    else if(i >= 5) {
-                        space.setPiece(new Piece(Piece.Color.RED));
-                    }
-                }
-            }
-        }
-    }
 
-    public Space[][] getBoardRed() {
+
+    public Board getBoardRed() {
         return board;
     }
 
-    public Space[][] getBoardWhite() {
-        Space[][] flipped = new Space[BOARD_SIZE][BOARD_SIZE];
-        for (int i = 0; i < BOARD_SIZE; i++) {
-            for (int j = 0; j < BOARD_SIZE; j++) {
-                flipped[BOARD_SIZE - 1 - i][BOARD_SIZE - 1 - j] = board[i][j];
-            }
-        }
-        return flipped;
-    }
-
-    public List<Space> getBoardRedIter() {
-        List<Space> iter = new ArrayList<Space>();
-        for(int i = 0; i < BOARD_SIZE; i++) {
-            for(int j = 0; j < BOARD_SIZE; j++) {
-                iter.add(board[i][j]);
-            }
-        }
-        return iter;
-    }
-
-    public List<Space> getBoardWhiteIter() {
-        return null;
+    public Board getBoardWhite() {
+        return board.flipped();
     }
 
     public String toString() {
         String toReturn = "";
-        for(int i = 0; i < BOARD_SIZE; i++) {
-            for(int j = 0; j < BOARD_SIZE; j++) {
-                if(board[i][j].isValid()) {
-                    if(board[i][j].hasPiece()) {
-                        if(board[i][j].isPieceRed()) {
+        Space[][] boardArray = board.getBoard();
+        for(int i = 0; i < Board.BOARD_SIZE; i++) {
+            for(int j = 0; j < Board.BOARD_SIZE; j++) {
+                if(boardArray[i][j].isValid()) {
+                    if(boardArray[i][j].hasPiece()) {
+                        if(boardArray[i][j].isPieceRed()) {
                             toReturn += "[R]";
                         }
                         else {
