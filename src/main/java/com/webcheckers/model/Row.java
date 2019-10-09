b9094100ddcd8bdbbb05ad16df44877743aac4db
Giovanni <gtm7712@rit.edu>
2019-10-09 09:33:26 -0400

    Implement Row class

--- src/main/java/com/webcheckers/model/Row.java
+++ src/main/java/com/webcheckers/model/Row.java
@@ -2,5 +2,23 @@ package com.webcheckers.model;
 
 public class Row {
 
+    private Space[] row;
+    private int index;
+
+    public Row(Space[] row, int index) {
+        this.row = row;
+    }
+
+    public Space[] getRow() {
+        return row;
+    }
+
+    public Space get(int index) {
+        return row[index];
+    }
+
+    public int getIndex() {
+        return index;
+    }
 
 }
