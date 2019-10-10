c8f1e777ff4dde34571590846e48dfa172bfbf77
Giovanni <gtm7712@rit.edu>
2019-10-10 13:42:46 -0400

    Add JavaDoc Comments

--- src/main/java/com/webcheckers/model/Game.java
+++ src/main/java/com/webcheckers/model/Game.java
@@ -1,38 +1,71 @@
 package com.webcheckers.model;
 
+/**
+ * The Game
+ */
 public class Game {
     private Board board;
     private Player redPlayer;
     private Player whitePlayer;
 
+    /**
+     * Create a new Game
+     */
     public Game() {
         this.board = new Board();
     }
 
+    /**
+     * 
+     * @return The white player
+     */
     public Player getWhitePlayer(){
         return whitePlayer;
     }
 
+    /**
+     * 
+     * @return The red player
+     */
     public Player getRedPlayer(){
         return redPlayer;
     }
 
+    /**
+     * Set the red player
+     * @param p New red player
+     */
     public void setRedPlayer(Player p){
         this.redPlayer = p;
     }
 
+    /**
+     * Set the white player
+     * @param p New white player
+     */
     public void setWhitePlayer(Player p){
         this.whitePlayer = p;
     }
 
+    /**
+     * 
+     * @return The board oriented for the Red player 
+     */
     public Board getBoardRed() {
         return board;
     }
 
+    /**
+     * 
+     * @return The board oriented for the White player
+     */
     public Board getBoardWhite() {
         return board.flipped();
     }
 
+    /**
+     * @return The board represented in String format
+     */
     public String toString() {
         String toReturn = "";
         Space[][] boardArray = board.getBoard();
@@ -61,6 +94,10 @@ public class Game {
     }
 
 
+    /**
+     * Main entry point for the Game
+     * @param args
+     */
     public static void main(String[] args) {
         // Stuff for testing
         Game game = new Game();
--- src/main/java/com/webcheckers/model/Space.java
+++ src/main/java/com/webcheckers/model/Space.java
@@ -1,5 +1,8 @@
 package com.webcheckers.model;
 
+/**
+ * Represents a space on the game board
+ */
 public class Space {
 
     private int row;
@@ -8,32 +11,61 @@ public class Space {
 
     private Piece piece = null;
 
+    /**
+     * Create a new Space
+     * @param row The row this space is in
+     * @param col The column this space is in
+     * @param isValid Can a piece be placed on this Space?
+     */
     public Space(int row, int col, boolean isValid) {
         this.row = row;
         this.col = col;
         this.isValid = isValid;
     }
 
+    /**
+     * Set the piece
+     * @param piece The piece on this space
+     */
     public void setPiece(Piece piece) {
         this.piece = piece;
     }
 
+    /**
+     * 
+     * @return The piece on this Space
+     */
     public Piece getPiece() {
         return piece;
     }
 
+    /**
+     * 
+     * @return True if the space has a piece
+     */
     public boolean hasPiece() {
         return piece != null;
     }
 
+    /**
+     * @return True if there is a piece and it is red
+     */
     public boolean isPieceRed() {
         return hasPiece() && piece.isRed();
     }
 
+    /**
+     * 
+     * @return True if a piece can be placed on this Space
+     */
     public boolean isValid() {
         return isValid;
     }
 
+    /**
+     * 
+     * @return The row this space is in
+     */
     public int getRow(){
         return row;
     }
