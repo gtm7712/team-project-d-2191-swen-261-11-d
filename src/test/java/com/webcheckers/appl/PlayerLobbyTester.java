d79e6dd8439b931d35072533ce6ddc92ebcfb457
Brandon <bxc4081@rit.edu>
2019-10-20 21:16:16 -0400

    added documentation to PlayerLobbyTester

--- src/test/java/com/webcheckers/appl/PlayerLobbyTester.java
+++ src/test/java/com/webcheckers/appl/PlayerLobbyTester.java
@@ -5,33 +5,50 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.HashMap;
 
+import com.webcheckers.model.Board;
 import com.webcheckers.model.Player;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Unit test suite for {@link PlayerLobby} component
+ * 
+ * @author: Brandon Chen
+ */
 @Tag("application-tier")
 public class PlayerLobbyTester{
 
+    /**
+     * Test for usernames that only have numbers
+     */
     @Test
     public void numericOnly(){
         final PlayerLobby CuT = new PlayerLobby();
         assertEquals(CuT.checkUsername("123"), 0);
     }
 
+    /**
+     * Test for usernames that are blank
+     */
     @Test
     public void emptyName(){
         final PlayerLobby CuT = new PlayerLobby();
         assertEquals(CuT.checkUsername(""), 0);
     }
     
+    /**
+     * Test for usernames that are null
+     */
     @Test
     public void nullName(){
         final PlayerLobby CuT = new PlayerLobby();
         assertEquals(CuT.checkUsername(null), 0);
     }
 
+    /**
+     * Test for usernames that are taken by other users
+     */
     @Test
     public void notAvailable(){
         final PlayerLobby CuT = new PlayerLobby();
@@ -39,12 +56,18 @@ public class PlayerLobbyTester{
         assertEquals(CuT.checkUsername("test"), 1);
     }
     
+    /**
+     * Test for a valid name
+     */
     @Test
     public void goodName(){
         final PlayerLobby CuT = new PlayerLobby();
         assertEquals(CuT.checkUsername("a123"), 2);
     }
     
+    /**
+     * Test for adding a username to the lobby
+     */
     @Test
     public void addUsernameTest(){
         final PlayerLobby CuT = new PlayerLobby();
@@ -52,6 +75,9 @@ public class PlayerLobbyTester{
         assertNotNull(CuT.getUsernames(), "Player Lobby should not be null");
     }
 
+    /**
+     * Test to see if the number of people are correct
+     */
     @Test
     public void countPlayerTest(){
         final PlayerLobby CuT = new PlayerLobby();
@@ -61,6 +87,9 @@ public class PlayerLobbyTester{
         assertEquals(CuT.countPlayers(), test.size());
     }
 
+    /**
+     * Test to see if the player gotten from getPlayer is correct
+     */
     @Test
     public void getPlayerTest(){
         final PlayerLobby CuT = new PlayerLobby();
@@ -68,6 +97,9 @@ public class PlayerLobbyTester{
         assertEquals(CuT.getPlayer("Brandon"), new Player("Brandon"));
     }
 
+    /**
+     * Test to see if the hashmap returned is correct
+     */
     @Test
     public void getUsernamesTest(){
         final PlayerLobby CuT = new PlayerLobby();
