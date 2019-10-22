15421f4a2001a0e79a25ce9f9a5403b314920517
chiefcollins <kscollins31@gmail.com>
2019-10-22 14:57:41 -0400

    Created PlayerTest

---
+++ src/test/java/com/webcheckers/model/PlayerTest.java
@@ -0,0 +1,133 @@
+package com.webcheckers.model;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * The unit testing suite for {@link Player}
+ * 
+ * @author Kyle Collins
+ */
+@Tag ("Model-tier")
+public class PlayerTest {
+
+
+    private Player player1;
+    private Player player2;
+    private String playerName;
+    private Game game;
+    private Board board;
+    private Board flipped;
+    
+    /**
+     * Creates two players and a game
+     */
+    @BeforeEach
+    public void setup(){
+
+        this.player1 = new Player("Player_1");
+        this.player2 = new Player("Player_2");
+
+        this.playerName = "Player_1";
+
+        this.game = new Game();
+        this.board = this.game.getBoardRed();
+        this.flipped = this.game.getBoardWhite();
+    }
+
+    /**
+     * Test getName 
+     */
+    @Test
+    public void getNameTest() {
+        assertTrue(player1.getName() == playerName);
+    }
+
+    /**
+     * Test the inGame and isInGame functions
+     */
+    @Test
+    public void inGameTest() {
+
+        this.player1.inGame(true);
+        assertTrue(player1.isInGame()== true);
+        assertTrue(player2.isInGame() == false);
+
+    }
+
+    /**
+     * Test the getter and setter for opponent
+     */
+    @Test
+    public void opponentTest() {
+
+        this.player1.setOpponent(player2);
+        assertTrue(player1.getOpponent() == player2);
+
+        this.player2.setOpponent(player1);
+        assertTrue(player2.getOpponent() == player1);
+
+    }
+
+    /**
+     * Test the getBoard function
+     */
+    @Test
+    public void getBoardTest() {
+
+        assertTrue(player1.getBoard() == board);
+
+    }
+
+    /**
+     * Test the getFlippedBoard
+     */
+    @Test
+    public void getFlippedBoardTest() {
+
+        assertTrue(player1.getFlippedBoard() == flipped);
+
+    }
+
+    /**
+     * Test getter and setter for Game
+     */
+    @Test
+    public void gameTest() {
+        
+        this.player1.setGame(game);
+        assertTrue(player1.getGame() == game);
+
+    }
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+    
+
+
+
+
+
+
+
+  
+
+
+
+
+}
