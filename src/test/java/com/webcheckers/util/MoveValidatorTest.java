2150f569c606982e8f0adb17510ddfc20b6277ed
Giovanni <gtm7712@rit.edu>
2019-10-26 13:18:16 -0400

    Add jump wrong piece unit test

--- src/test/java/com/webcheckers/util/MoveValidatorTest.java
+++ src/test/java/com/webcheckers/util/MoveValidatorTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.webcheckers.model.Board;
 import com.webcheckers.model.Move;
+import com.webcheckers.model.Piece;
 import com.webcheckers.model.Position;
+import com.webcheckers.model.Piece.Color;
 import com.webcheckers.util.MoveValidator.TurnResult;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -32,16 +34,33 @@ public class MoveValidatorTest {
      */
     @Test
     public void noJump() {
-        Move m = new Move(new Position(1, 1), new Position(2, 2));assertEquals(mv.validateMove(m), TurnResult.COMPLETE);
+        Move m = new Move(new Position(1, 1), new Position(2, 2));
         assertEquals(mv.validateMove(m), TurnResult.COMPLETE);
     }
 
     /**
-     * Test a move where an invalid jump is made
+     * Test a move where an invalid jump is made (nothing to jump)
      */
     @Test
     public void invalidJumpNoPiece() {
         Move m = new Move(new Position(1, 1), new Position(3, 3));
+        assertEquals(mv.validateMove(m), TurnResult.FAIL);
+    }
+
+    /**
+     * Test a move where an invalid jump is made (same color jumped)
+     */
+    @Test
+    public void invalidJumpWrongPiece() {
+        Piece p = new Piece(Color.RED);
+        Piece j = new Piece(Color.RED);
+
+        Move m = new Move(new Position(1, 1), new Position(3, 3));
+        
+        board.getSpace(1, 1).setPiece(p);
+        board.getSpace(2, 2).setPiece(j);
+        board.getSpace(3, 3).setPiece(null);
+
         assertEquals(mv.validateMove(m), TurnResult.FAIL);
     }
 
